817e8f4e0d8f52901941dc2e3cbc36dbc502462b
sathish <jeevajoker2005@gmail.com>
2025-10-08 00:06:36 +0530

    updated

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
-let time = document.querySelector('h1')
-let red = document.querySelector('.red')
-let yellow = document.querySelector('.yellow')
-let green = document.querySelector('.green')
+let time = document.getElementById('timer');
+let red = document.querySelector('.red');
+let yellow = document.querySelector('.yellow');
+let green = document.querySelector('.green');
 
-let count = 60
-
+let count = 60;
+function addOn(){
+    window.location.href="./traffic.html"
+}
 function number(){
-    count -= 1
-    time.textContent = count
+    count -= 1;
+    time.textContent = count;
+
     if(count === 59){
-        green.style.backgroundColor = 'lime '
-        yellow.style.backgroundColor = 'white'
-        red.style.backgroundColor = 'white'
+        green.style.backgroundColor = 'lime';
+        yellow.style.backgroundColor = 'white';
+        red.style.backgroundColor = 'white';
     }
     if(count === 5){
-        green.style.backgroundColor = 'white'
-        yellow.style.backgroundColor = 'yellow'
-        red.style.backgroundColor = 'white'
+        green.style.backgroundColor = 'white';
+        yellow.style.backgroundColor = 'yellow';
+        red.style.backgroundColor = 'white';
     }
     if(count === 1){
-        green.style.backgroundColor = 'white'
-        yellow.style.backgroundColor = 'white'
-        red.style.backgroundColor = 'red'
+        green.style.backgroundColor = 'white';
+        yellow.style.backgroundColor = 'white';
+        red.style.backgroundColor = 'red';
     }
     if(count === 0){
-        count = 120
+        count = 120;
     }
     if(count === 65){
-         green.style.backgroundColor = 'white'
-        yellow.style.backgroundColor = 'yellow'
-        red.style.backgroundColor = 'white'
+        green.style.backgroundColor = 'white';
+        yellow.style.backgroundColor = 'yellow';
+        red.style.backgroundColor = 'white';
     }
-
 }
 
 setInterval(() => {
-    number()
-},1000)
+    number();
+}, 1000);
